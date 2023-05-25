d08ba35350be7a3c1581a5bf180998a4797e1433
Jake Earing <jakeearing7@gmail.com>
2023-05-25 19:13:28 -0400

    Big catalog categories update
    The filtering functionality has now been added back, bringing the user to new URLS based on the category. However, the subcategories urls are currently not displaying any products at the moment.

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -4,57 +4,66 @@ import Products from '../components/ProductSquare';
 import '../assets/styles/catalog.css';
 
 function Catalog({ products, images }) {
+  // State variables
   const { category, subcategory } = useParams();
   const [searchTerm, setSearchTerm] = useState('');
   const [sortOption, setSortOption] = useState('modelNumber');
+  const [itemsPerPage, setItemsPerPage] = useState(() => {
+    const savedItemsPerPage = localStorage.getItem('itemsPerPage');
+    return savedItemsPerPage ? JSON.parse(savedItemsPerPage) : 25;
+  });
+
+  // Constants
   const sortOptions = [
     { value: 'name', label: 'Name' },
     { value: 'modelNumber', label: 'Model Number' },
     { value: 'price', label: 'Price' },
   ];
-  const [itemsPerPage, setItemsPerPage] = useState(() => {
-    const savedItemsPerPage = localStorage.getItem('itemsPerPage');
-    return savedItemsPerPage ? JSON.parse(savedItemsPerPage) : 25;
-  });
   const itemsPerPageOptions = [10, 25, 50, 100, 'All'];
 
+  // Update local storage when itemsPerPage changes
   useEffect(() => {
     localStorage.setItem('itemsPerPage', JSON.stringify(itemsPerPage));
   }, [itemsPerPage]);
 
+  // Filter products based on category and search term
   const filteredProducts = products.filter((product) => {
     const nameMatch = product.name.toLowerCase().includes(searchTerm.toLowerCase());
     const modelMatch = product.modelNumber.toLowerCase().includes(searchTerm.toLowerCase());
-  
+
     if (category === 'all') {
       return nameMatch || modelMatch;
     } else if (category === 'electrical') {
       return (
         product.category.toLowerCase() === 'electrical' &&
-        (subcategory === null || product.subCategory.toLowerCase() === subcategory)
+        (subcategory === undefined || product.subCategory.toLowerCase() === subcategory)
       );
     } else if (category === 'hardware') {
       return (
         product.category.toLowerCase() === 'hardware' &&
-        (subcategory === null || product.subCategory.toLowerCase() === subcategory)
+        (subcategory === undefined || product.subCategory.toLowerCase() === subcategory)
       );
     } else {
       return product.category.toLowerCase() === category;
     }
   });
 
+  // Handle sort option change
   const handleSortChange = (event) => {
     setSortOption(event.target.value);
   };
 
+  // Handle search form submission
   const handleSearch = (event) => {
     event.preventDefault();
   };
 
+  // Handle search term change
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
+  // Sort products based on sort option
   const sortedProducts = [...filteredProducts].sort((a, b) => {
     if (sortOption === 'name') {
       return a.name.localeCompare(b.name);
@@ -67,6 +76,7 @@ function Catalog({ products, images }) {
     }
   });
 
+  // Handle items per page change
   const handleChangeItemsPerPage = (event) => {
     setItemsPerPage(event.target.value);
   };
@@ -77,14 +87,26 @@ function Catalog({ products, images }) {
   return (
     <div>
       <div className="categories">
-        <Link to="/category/electrical">Electrical</Link>
-        <Link to="/category/hardware">Hardware</Link>
         <Link to="/category/all">All Products</Link>
+        <Link to="/category/electrical">Electrical</Link>
+        <p>
+          <Link to="/category/electrical/wiring">Wiring & Wiring accessories</Link>
+        </p>
+        <p>
+          <Link to="/category/electrical/cable-clips">Cable Clips & Staples</Link>
+        </p>
+        <p>
+          <Link to="/category/electrical/raceway">Raceway & Raceway Accessories</Link>
+        </p>
+        <Link to="/category/hardware">Hardware</Link>
+        <p>
+          <Link to="/category/hardware/anchors">Anchors Screws & Kits</Link>
+        </p>
       </div>
       <div className="currentCategory">
         {subcategory && (
           <h3>
-            <Link to={`/category/${category}`}>{category}</Link> - {subcategory}
+            <Link to={`/category/${category}/${subcategory}`}>{category}</Link> - {subcategory}
           </h3>
         )}
         {!subcategory && (
