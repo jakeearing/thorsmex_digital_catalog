4cfa690b1225f064ab93c26e187ff0c9fd32ffb1
Jake Earing <jakeearing7@gmail.com>
2023-05-25 15:09:34 -0400

    Footer content and style update

--- src/components/nav/Footer.js
+++ src/components/nav/Footer.js
@@ -1,22 +1,36 @@
-import '../../assets/styles/footer.css';
 import { Link } from 'react-router-dom';
+import '../../assets/styles/footer.css';
 import cimpoLogo from '../../assets/images/logos/cimpo-logo.jpg';
 
 export default function Footer() {
     return (
-        <div className="footer-container">
+      <div className="footer-container">
+        <div className="footer">
+          <div className="left-nav">
+            <nav>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/contact-us">Contact</Link></li>
+                <li><Link to="/about-us">About</Link></li>
+              </ul>
+            </nav>
+          </div>
+          <div className="footer-logo">
             <Link to="/">
-                <img className="footer-photo" src={cimpoLogo} alt="Charlotte Imports logo" />
+              <img src={cimpoLogo} alt="Charlotte Imports Logo" />
             </Link>
-            <div className="footer-nav">
-                <nav>
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/">Contact Us</Link></li>
-                        <li><Link to="/">About Us</Link></li>
-                    </ul>
-                </nav>
+          </div>
+          <div className="right-nav">
+            <div className="social-media">
+              <a href="https://www.youtube.com/channel/UCKvQ-LQyrH94RGB0VAoSY0Q"><img src={process.env.PUBLIC_URL + "/svg-icons/social-media/youtube-logo.svg"} alt="Youtube Icon" /></a>
+              <a href="https://www.instagram.com/clt.imp/"><img src={process.env.PUBLIC_URL + "/svg-icons/social-media/instagram-logo.svg"} alt="Instagram Icon" /></a>
             </div>
+            <div className="terms">
+              <Link to="/terms-of-use">Terms of Use</Link>
+              <Link to="/privacy-policy">Privacy Policy</Link>
+            </div>
+          </div>
         </div>
+      </div>
     );
-}
+  }
