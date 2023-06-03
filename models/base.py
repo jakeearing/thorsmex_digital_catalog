731639d7fd104a982581eae825004fb038014093
Jake Earing <jakeearing7@gmail.com>
2023-06-03 13:29:05 -0400

    Reorganized database model

--- models/base.py
+++ models/base.py
@@ -11,21 +11,13 @@ connect('thorsmex_catalog', host='mongodb://localhost:27017/')
 # MongoDB products model created
 class Products(Document):
     name = StringField(required=True)
+    modelNumber = StringField(required=True)
     price_indv = DecimalField(precision=2,required=True)
     price_box = DecimalField(precision=2,required=True)
-    price_pallet = DecimalField(precision=2,required=True)
-    stock = ListField(IntField())
+    price_pallet = DecimalField(precision=2,required=True) 
     count_indv = IntField()
     count_box = IntField()
     count_pallet = IntField()
-    gtin = IntField(required=True)
-    modelNumber = StringField(required=True)
-    category = StringField(required=True)
-    subCategory = StringField(required=True)
-    description = StringField()
-    details = StringField()
-    specs = StringField()
-    product_sheet = StringField()
     height_indv = DecimalField(precision=2)
     width_indv = DecimalField(precision=2)
     weight_indv = DecimalField(precision=2)
@@ -35,6 +27,14 @@ class Products(Document):
     height_pallet = DecimalField(precision=2)
     width_pallet = DecimalField(precision=2)
     weight_pallet = DecimalField(precision=2)
+    stock = ListField(IntField())
+    gtin = IntField(required=True)
+    category = StringField(required=True)
+    subCategory = StringField(required=True)
+    description = StringField()
+    details = StringField()
+    specs = StringField()
+    product_sheet = StringField()
     english = StringField(null=True)
     stock_NC = IntField()
     stock_TX = IntField()
@@ -52,21 +52,13 @@ def import_data():
     for index, row in df.iterrows():
         product = Products(
             name=row['name'],
+            modelNumber=row['model_number'],
             price_indv=row['price_indv'],
             price_box=row['price_box'],
             price_pallet=row['price_pallet'],
-            stock=[row['stock_NC'], row['stock_TX'], row['stock_MX']],
             count_indv=row['count_indv'],
             count_box=row['count_box'],
             count_pallet=row['count_pallet'],
-            gtin=row['gtin'],
-            modelNumber=row['model_number'],
-            category=row['category'],
-            subCategory=row['sub_category'],
-            description=row['description'],
-            details=row['details'],
-            specs=row['specs'],
-            product_sheet=row['product_sheet'],
             height_indv=row['height_indv'],
             width_indv=row['width_indv'],
             weight_indv=row['weight_indv'],
@@ -76,6 +68,14 @@ def import_data():
             height_pallet=row['height_pallet'],
             width_pallet=row['width_pallet'],
             weight_pallet=row['weight_pallet'],
+            stock=[row['stock_NC'], row['stock_TX'], row['stock_MX']],
+            gtin=row['gtin'],
+            category=row['category'],
+            subCategory=row['sub_category'],
+            description=row['description'],
+            details=row['details'],
+            specs=row['specs'],
+            product_sheet=row['product_sheet'],
             english=row['english'],
             stock_NC=row['stock_NC'],
             stock_TX=row['stock_TX'],
