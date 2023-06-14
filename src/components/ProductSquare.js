b29f21a14d1264c5a939bca0c69541e2b1016d74
Jake Earing <jakeearing7@gmail.com>
2023-06-14 19:30:43 -0400

    Product image in catalog now defaults to the main image

--- src/components/ProductSquare.js
+++ src/components/ProductSquare.js
@@ -3,7 +3,15 @@ import { Link } from 'react-router-dom';
 
 export default function Products({ product, images }) {
   const { name, unit_cost, modelNumber, category, subCategory } = product;
-  const productImage = images[Object.keys(images).find(key => key.startsWith(modelNumber))] || images['notfound.jpg'];
+
+  // Look for the product image that has "main" in the name and load it as the product's image
+  // If it is not found, load the first image with the model number that is found
+  // If that is not found either, simply load the notfound.jpg
+  const productImage =
+  images[
+    Object.keys(images).find(key => key.includes('main') && key.startsWith(modelNumber)) ||
+    Object.keys(images).find(key => key.startsWith(modelNumber))
+  ] || images['notfound.jpg'];
 
   return (
     <Link to={{ pathname: `/${category}/${subCategory}/${modelNumber}`, state: { modelNumber } }}>
