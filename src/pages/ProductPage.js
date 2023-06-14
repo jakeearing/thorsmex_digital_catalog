c753b0a4d4308cfc3dfdb054b836c40fe3360501
Jake Earing <jakeearing7@gmail.com>
2023-06-14 19:06:52 -0400

    All product images can now be scrolled through on the product Page

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -9,9 +9,11 @@ export default function Product({ products, images }) {
   const { category, subCategory, modelnumber } = useParams();
   const [product, setProduct] = useState(null);
   const [activeTab, setActiveTab] = useState('details');
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
-    const storedProducts = localStorage.getItem("products");
+    const storedProducts = localStorage.getItem('products');
     if (storedProducts) {
       const parsedProducts = JSON.parse(storedProducts);
       const storedProduct = parsedProducts.find(
@@ -29,10 +31,10 @@ export default function Product({ products, images }) {
       .then((res) => {
         setProduct(res.data);
         // Update the locally stored products data
-        const storedProducts = localStorage.getItem("products");
+        const storedProducts = localStorage.getItem('products');
         const parsedProducts = storedProducts ? JSON.parse(storedProducts) : [];
         const updatedProducts = [...parsedProducts, res.data];
-        localStorage.setItem("products", JSON.stringify(updatedProducts));
+        localStorage.setItem('products', JSON.stringify(updatedProducts));
       })
       .catch((err) => {
         console.error(err);
@@ -43,122 +45,192 @@ export default function Product({ products, images }) {
     setActiveTab(tabName);
   };
 
-    const { name, gtin, description, details, specs, product_sheet, price_indv,
-      price_box, price_pallet, unit_cost, count_indv, count_box, count_pallet, height_indv,
-      width_indv, length_indv, weight_indv, height_box, width_box, length_box,
-      weight_box, height_pallet, width_pallet, length_pallet, weight_pallet,
-      packaging_type, english_packaging, stock } = product || {};
+  const handlePrevImage = () => {
+    setActiveImageIndex((prevIndex) => {
+      const lastIndex = productImages.length - 1;
+      return prevIndex === 0 ? lastIndex : prevIndex - 1;
+    });
+  };
+  
+  const handleNextImage = () => {
+    setActiveImageIndex((prevIndex) => {
+      const lastIndex = productImages.length - 1;
+      return prevIndex === lastIndex ? 0 : prevIndex + 1;
+    });
+  };
+  
+  const handleImageClick = () => {
+    setIsModalOpen(true);
+  };
 
-    const productImage =
-      images[Object.keys(images).find((key) => key.startsWith(modelnumber))] || images['notfound.jpg'];
+  const { name, gtin, description, details, specs, product_sheet, price_indv,
+    price_box, price_pallet, unit_cost, count_indv, count_box, count_pallet, height_indv,
+    width_indv, length_indv, weight_indv, height_box, width_box, length_box,
+    weight_box, height_pallet, width_pallet, length_pallet, weight_pallet,
+    packaging_type, english_packaging, stock } = product || {};
 
-    return (
-      <div className="product-page-container">
-        <div className="product">
-          <div className="product-image">
-            <img src={productImage} alt={name} />
-          </div>
-          <div className="product-details-container">
-            <div className="product-name">
-              <p>{name}</p>
-              <p>Model Number: {modelnumber}</p>
-            </div>
-            <div className="details-contact">
-              <div className="product-details">
-                <p>
-                  <b>Individual Price ({count_indv} count):</b> ${price_indv}
-                </p>
-                <p>
-                  <b>Box Price ({count_box} pieces):</b> ${price_box}
-                </p>
-                <p>
-                  <b>Pallet Price ({count_pallet} boxes):</b> ${price_pallet}
-                </p>
-                <p>
-                  <b>Unit Cost:</b> ${unit_cost}
-                </p>
-                {product_sheet && product_sheet.endsWith(".pdf") ? (
-                  <p>
-                    <a
-                      href={require(`../assets/images/product-info-sheets/${product_sheet}`)}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Click here for product sheet
-                    </a>
-                  </p>
-                ) : (
-                  <p>No Product Sheet Available</p>
-                )}
-              </div>
-              <div className="contact-details">
-                <Contact />
-              </div>
-            </div>
-            <div className="product-description">
-              <p>{description}</p>
-            </div>
-          </div>
+  const folderName = modelnumber.toString();
+
+  // Load all the images inside the model number folder
+  const imageContext = require.context(
+    '../assets/images/product-images',
+    true,
+    /\.(png|jpe?g|gif|svg)$/
+  );
+  const imagePaths = imageContext.keys().filter((key) =>
+    key.startsWith(`./${folderName}/`)
+  );
+  const productImages = imagePaths.map((path) => {
+    const image = imageContext(path);
+    return image.default || image;
+  });
+
+  const showPrevImageArrow = productImages.length > 1;
+  const showNextImageArrow = productImages.length > 1;
+
+  return (
+    <div className="product-page-container">
+      <div className="product">
+        <div className="product-image">
+          <img
+            src={productImages[activeImageIndex]}
+            alt={name}
+            onClick={handleImageClick}
+            className="clickable"
+          />
+          {showPrevImageArrow && (
+            <button onClick={handlePrevImage}>&#60;</button>
+          )}
+          {showNextImageArrow && (
+            <button onClick={handleNextImage}>&#62;</button>
+          )}
         </div>
-        <div className="product-secondary-container">
-          <div className="item-specs">
-            <button
-              className={activeTab === 'details' ? 'active' : ''}
-              onClick={() => handleTabClick('details')}
-            >
-              Details
-            </button>
-            <button
-              className={activeTab === 'item-specifications' ? 'active' : ''}
-              onClick={() => handleTabClick('item-specifications')}
-            >
-              Item Specifications
-            </button>
-            <button
-              className={activeTab === 'more-information' ? 'active' : ''}
-              onClick={() => handleTabClick('more-information')}
-            >
-              More Information
-            </button>
+        <div className="product-details-container">
+          <div className="product-name">
+            <p>{name}</p>
+            <p>Model Number: {modelnumber}</p>
           </div>
-
-          <div className="tab-content">
-            {activeTab === 'details' && (
-              <ul>
-                {details &&
-                  details.split(';').map((item, index) => (
-                    <li key={index}>{item.trim()}</li>
-                  ))}
-              </ul>
-            )}
-
-            {activeTab === 'item-specifications' && (
-              <ul>
-                {specs &&
-                  specs.split(';').map((item, index) => (
-                    <li key={index}>{item.trim()}</li>
-                  ))}
-              </ul>
-            )}
-
-            {activeTab === 'more-information' && (
-              <ul>
-                <li>Packaging Type: {packaging_type}</li>
-                <li>English Packaging: {english_packaging}</li>
-                <li>Individual Dimensions: {height_indv} x {width_indv} x {length_indv} inches</li>
-                <li>Individual Weight: {weight_indv} pounds</li>
-                <li>Box Dimensions: {height_box} x {width_box} x {length_box} inches</li>
-                <li>Box Weight: {weight_box} pounds</li>
-                <li>Pallet Dimensions: {height_pallet} x {width_pallet} x {length_pallet} inches</li>
-                <li>Pallet Weight: {weight_pallet} pounds</li>
-                <li>Barcode Number: {gtin}</li>
-              </ul>
-            )}
+          <div className="details-contact">
+            <div className="product-details">
+              <p>
+                <b>Individual Price ({count_indv} count):</b> ${price_indv}
+              </p>
+              <p>
+                <b>Box Price ({count_box} pieces):</b> ${price_box}
+              </p>
+              <p>
+                <b>Pallet Price ({count_pallet} boxes):</b> ${price_pallet}
+              </p>
+              <p>
+                <b>Unit Cost:</b> ${unit_cost}
+              </p>
+              {product_sheet && product_sheet.endsWith('.pdf') ? (
+                <p>
+                  <a
+                    href={require(`../assets/images/product-info-sheets/${product_sheet}`)}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Click here for product sheet
+                  </a>
+                </p>
+              ) : (
+                <p>No Product Sheet Available</p>
+              )}
+            </div>
+            <div className="contact-details">
+              <Contact />
+            </div>
           </div>
-          <div className="product-options">
-            {product && <ProductOptions products={products} images={images} currentModelNumber={modelnumber} />}
+          <div className="product-description">
+            <p>{description}</p>
           </div>
         </div>
       </div>
-    );
-  }
+      <div className="product-secondary-container">
+        <div className="item-specs">
+          <button
+            className={activeTab === 'details' ? 'active' : ''}
+            onClick={() => handleTabClick('details')}
+          >
+            Details
+          </button>
+          <button
+            className={activeTab === 'item-specifications' ? 'active' : ''}
+            onClick={() => handleTabClick('item-specifications')}
+          >
+            Item Specifications
+          </button>
+          <button
+            className={activeTab === 'more-information' ? 'active' : ''}
+            onClick={() => handleTabClick('more-information')}
+          >
+            More Information
+          </button>
+        </div>
+
+        <div className="tab-content">
+          {activeTab === 'details' && (
+            <ul>
+              {details &&
+                details.split(';').map((item, index) => (
+                  <li key={index}>{item.trim()}</li>
+                ))}
+            </ul>
+          )}
+
+          {activeTab === 'item-specifications' && (
+            <ul>
+              {specs &&
+                specs.split(';').map((item, index) => (
+                  <li key={index}>{item.trim()}</li>
+                ))}
+            </ul>
+          )}
+
+          {activeTab === 'more-information' && (
+            <ul>
+              <li>Packaging Type: {packaging_type}</li>
+              <li>English Packaging: {english_packaging}</li>
+              <li>
+                Individual Dimensions: {height_indv} x {width_indv} x{' '}
+                {length_indv} inches
+              </li>
+              <li>Individual Weight: {weight_indv} pounds</li>
+              <li>
+                Box Dimensions: {height_box} x {width_box} x {length_box} inches
+              </li>
+              <li>Box Weight: {weight_box} pounds</li>
+              <li>
+                Pallet Dimensions: {height_pallet} x {width_pallet} x{' '}
+                {length_pallet} inches
+              </li>
+              <li>Pallet Weight: {weight_pallet} pounds</li>
+              <li>Barcode Number: {gtin}</li>
+            </ul>
+          )}
+        </div>
+        <div className="product-options">
+          {product && (
+            <ProductOptions
+              products={products}
+              images={images}
+              currentModelNumber={modelnumber}
+            />
+          )}
+        </div>
+      </div>
+      {isModalOpen && (
+        <div className="image-modal" onClick={() => setIsModalOpen(false)}>
+          <div className="image-modal-content">
+            <img
+              src={images[activeImageIndex]}
+              alt={name}
+              onClick={(e) => e.stopPropagation()}
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
