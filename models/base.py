1ea63f38fe625ee26923f4f32cc7199b6ed72ff0
jakeearing <jakeearing7@gmail.com>
2023-08-01 13:12:09 -0400

    Updated base.py to properly load .env file
    Was missing a line in the base.py file, causing the .env to not be loaded. This is now fixed.

--- models/base.py
+++ models/base.py
@@ -8,6 +8,9 @@ from dotenv import load_dotenv
 app = Flask(__name__)
 CORS(app)
 
+# Load environment variables from .env file
+load_dotenv()
+
 # Retrieve the MongoDB URI from the environment variable
 mongodb_uri = os.getenv('MONGODB_URI')
 if mongodb_uri is None:
