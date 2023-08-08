afbd0916d688ee1060166f8163992bdcb1415d56
jakeearing <jakeearing7@gmail.com>
2023-08-08 01:41:09 -0400

    /api/import endpoint fixed, now imports from csv files
    Fixed the /api/import endpoint, which was broken after the switch from flask to express. This now imports the data from a csv file named products.csv, instead of an xlsx file.

--- models/main.js
+++ models/main.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const xlsxPopulate = require('xlsx-populate');
+const fs = require('fs');
+const csv = require('csv-parser');
 
 dotenv.config();
 
@@ -19,16 +20,22 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
   });
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
   modelNumber: { type: String, required: true },
-  price_indv: { type: mongoose.Decimal128, required: true },
-  price_box: { type: mongoose.Decimal128, required: true },
-  price_pallet: { type: mongoose.Decimal128, required: true },
+  name: { type: String, required: true },
+  category: { type: String, required: true },
+  subCategory: { type: String, required: true },
+  description: { type: String },
+  details: { type: String },
+  specs: { type: String },
+  product_sheet: { type: String },
   msrp: { type: mongoose.Decimal128, required: true },
-  unit_cost: { type: mongoose.Decimal128 },
+  price_indv: { type: mongoose.Decimal128, required: true },
   count_indv: { type: Number },
+  price_box: { type: mongoose.Decimal128, required: true },
   count_box: { type: Number },
+  price_pallet: { type: mongoose.Decimal128, required: true },
   count_pallet: { type: Number },
+  unit_cost: { type: mongoose.Decimal128 },
   height_indv: { type: mongoose.Decimal128 },
   width_indv: { type: mongoose.Decimal128 },
   length_indv: { type: mongoose.Decimal128 },
@@ -42,79 +49,69 @@ const productSchema = new mongoose.Schema({
   length_pallet: { type: mongoose.Decimal128 },
   weight_pallet: { type: mongoose.Decimal128 },
   packaging_type: { type: String },
-  stock: [{ type: Number }],
-  gtin: { type: Number, required: true },
-  category: { type: String, required: true },
-  subCategory: { type: String, required: true },
-  description: { type: String },
-  details: { type: String },
-  specs: { type: String },
-  product_sheet: { type: String },
   english_packaging: { type: String },
-  stock_NC: { type: Number },
-  stock_TX: { type: Number },
-  stock_MX: { type: Number }
+  gtin: { type: Number, required: true },
 });
 
 // Create the 'Product' model using the schema
 const Product = mongoose.model('Product', productSchema, 'products');
 
-// Import data
+// Import data from CSV
 app.get('/api/import', async (req, res) => {
   try {
-    // Set XLSX file path and read XLSX file
-    const xlsxFilePath = 'products.xlsx';
-    const workbook = await xlsxPopulate.fromFileAsync(xlsxFilePath);
-    const sheet = workbook.sheet('products');
+    // Set CSV file path and read CSV file
+    const csvFilePath = './products.csv';
+    const products = [];
 
-    // Delete existing products
-    await Product.deleteMany();
+    fs.createReadStream(csvFilePath)
+      .pipe(csv())
+      .on('data', (row) => {
+        // Convert values to appropriate types
+        const product = new Product({
+          modelNumber: row.model_number,
+          name: row.name,
+          category: row.category,
+          subCategory: row.sub_category,
+          description: row.description,
+          details: row.details,
+          specs: row.specs,
+          product_sheet: row.product_sheet,
+          msrp: mongoose.Types.Decimal128.fromString(row.msrp),
+          price_indv: mongoose.Types.Decimal128.fromString(row.price_indv),
+          count_indv: Number(row.count_indv),
+          price_box: mongoose.Types.Decimal128.fromString(row.price_box),
+          count_box: Number(row.count_box),
+          price_pallet: mongoose.Types.Decimal128.fromString(row.price_pallet),
+          count_pallet: Number(row.count_pallet),
+          unit_cost: mongoose.Types.Decimal128.fromString(row.unit_cost),
+          height_indv: mongoose.Types.Decimal128.fromString(row.height_indv),
+          width_indv: mongoose.Types.Decimal128.fromString(row.width_indv),
+          length_indv: mongoose.Types.Decimal128.fromString(row.length_indv),
+          weight_indv: mongoose.Types.Decimal128.fromString(row.weight_indv),
+          height_box: mongoose.Types.Decimal128.fromString(row.height_box),
+          width_box: mongoose.Types.Decimal128.fromString(row.width_box),
+          length_box: mongoose.Types.Decimal128.fromString(row.length_box),
+          weight_box: mongoose.Types.Decimal128.fromString(row.weight_box),
+          height_pallet: mongoose.Types.Decimal128.fromString(row.height_pallet),
+          width_pallet: mongoose.Types.Decimal128.fromString(row.width_pallet),
+          length_pallet: mongoose.Types.Decimal128.fromString(row.length_pallet),
+          weight_pallet: mongoose.Types.Decimal128.fromString(row.weight_pallet),
+          packaging_type: row.packaging_type,
+          english_packaging: row.english_packaging,
+          gtin: Number(row.gtin),
+        });
 
-    // Insert data into MongoDB
-    const rows = sheet.usedRange().value();
-    for (const row of rows.slice(1)) {
-      const product = new Product({
-        name: row.name.replace("_x000D_", ""),
-        modelNumber: row.model_number,
-        price_indv: row.price_indv,
-        price_box: row.price_box,
-        price_pallet: row.price_pallet,
-        msrp: row.msrp,
-        unit_cost: row.unit_cost,
-        count_indv: row.count_indv,
-        count_box: row.count_box,
-        count_pallet: row.count_pallet,
-        height_indv: row.height_indv,
-        width_indv: row.width_indv,
-        length_indv: row.length_indv,
-        weight_indv: row.weight_indv,
-        height_box: row.height_box,
-        width_box: row.width_box,
-        length_box: row.length_box,
-        weight_box: row.weight_box,
-        height_pallet: row.height_pallet,
-        width_pallet: row.width_pallet,
-        length_pallet: row.width_pallet,
-        weight_pallet: row.weight_pallet,
-        packaging_type: row.packaging_type,
-        stock: [row.stock_NC, row.stock_TX, row.stock_MX],
-        gtin: row.gtin,
-        category: row.category,
-        subCategory: row.sub_category,
-        description: row.description.replace("_x000D_", ""),
-        details: row.details.replace("_x000D_", ""),
-        specs: row.specs.replace("_x000D_", ""),
-        product_sheet: row.product_sheet,
-        english_packaging: row.english_packaging,
-        stock_NC: row.stock_NC,
-        stock_TX: row.stock_TX,
-        stock_MX: row.stock_MX
+        products.push(product);
+      })
+      .on('end', async () => {
+        // Delete existing products
+        await Product.deleteMany();
+
+        // Insert data into MongoDB
+        await Product.insertMany(products);
+
+        res.send('Data imported successfully');
       });
-
-      await product.save();
-    }
-
-    res.send('Data imported successfully');
   } catch (error) {
     console.error('Error importing data:', error);
     res.status(500).send('Error importing data');
