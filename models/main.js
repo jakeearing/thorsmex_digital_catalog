9673985f2a12fb6be6095607536799c8072e239f
jakeearing <jakeearing7@gmail.com>
2023-08-07 22:08:37 -0400

    Backend port updated to always be 5000

--- models/main.js
+++ models/main.js
@@ -148,7 +148,7 @@ app.get('/api/products/:model_number', async (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
