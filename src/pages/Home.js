463af1f6b5a3be8696e82fd56f24fc9d7b07c502
jakeearing <jakeearing7@gmail.com>
2023-09-20 03:29:20 -0400

    Updated home images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,13 +7,11 @@ import '../assets/styles/home.css';
 import '../assets/styles/home.css';
 
 import image1 from '../assets/images/product-images/1101-01100/1101-01100 Thorquete tpo Bco main.jpg';
-import image2 from '../assets/images/product-images/1105-03000/1105-03000 main.jpg';
-import image3 from '../assets/images/product-images/1301-04000/1301-04000 BLISTER TPD BLANCO C.20 PZS main.jpg';
+import image2 from '../assets/images/product-images/1301-04000/1301-04000 BLISTER TPD BLANCO C.20 PZS main.jpg';
 import image4 from '../assets/images/product-images/2102-07240/2102-07240 JUEGO DE FIJACIÓN 7-10 main.jpg';
 import image5 from '../assets/images/product-images/2105-04100/2105-04100 SUJETHOR TK 20-26 main.jpg';
 import image6 from '../assets/images/product-images/3209-00300/3209-00300 main.jpg';
-import image7 from '../assets/images/product-images/3701-01000/3701-01000 Kit de fijación panel yeso.jpg';
-import image8 from '../assets/images/product-images/4700-06285/4700-06285 main.jpg';
+import image8 from '../assets/images/product-images/5001-01250/5001-01250 Canal TMK 0812 Blanco 1.10 m.jpg';
 import image9 from '../assets/images/product-images/5020-02001/5020-02001 ESQ. INT. 0812 CN 3 PZS main.jpg';
 import image10 from '../assets/images/product-images/5301-01250/5301-01250 CANAL TMK 1735 BCO DE 2.50 MTS main.jpg';
 import image11 from '../assets/images/product-images/9300-01252/9300-01252 FLEXIDUCTHO CAFE ROLLO 2.5 MTS main.jpg';
@@ -24,11 +22,9 @@ export default function Home() {
   const productImages = [
     image1,
     image2,
-    image3,
     image4,
     image5,
     image6,
-    image7,
     image8,
     image9,
     image10,
