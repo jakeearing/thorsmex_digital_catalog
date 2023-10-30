6cc0b75e92dc9ba74294138708d3a8f348b81308
jakeearing <jakeearing7@gmail.com>
2023-10-30 14:32:17 -0400

    Adjusted terms of use and privacy policy margins

--- src/pages/TermsOfUse.js
+++ src/pages/TermsOfUse.js
@@ -1,61 +1,63 @@
 import React, { useEffect } from 'react';
-import '../assets/styles/privacy-terms.css';
+import '../assets/styles/terms-privacy.css';
 
 export default function TermsOfUse() {
   return (
     <div className="terms-privacy-container">
-      <h1>Terms of Use</h1>
-  <ol>
-    <li>
-      <h2>Acceptance of Terms</h2>
-      <p>
-        By accessing and using the Charlotte Imports / THORSMEX, S.A de C.V. Digital Catalog website 
-        (hereinafter referred to as "the website"), you agree to comply with and be bound by the following 
-        terms and conditions of use. If you do not agree with any part of these terms, please refrain from using the website.
-      </p>
-    </li>
-    <li>
-      <h2>Account Creation</h2>
-      <p>
-        To access certain features of the website, you may be required to create an account. You are solely responsible for 
-        maintaining the confidentiality of your account information and for all activities that occur under your account. You 
-        must provide accurate and complete information during the account registration process. You are responsible for keeping 
-        your account information up to date and for promptly notifying us of any unauthorized use of your account or any other security breach.
-      </p>
-    </li>
-    <li>
-      <h2>Payment and Transactions</h2>
-      <p>
-        The website may offer a payment feature to facilitate the purchase of products or services. By making a payment, you 
-        agree to provide accurate and complete payment information and authorize the website to charge the specified amount to your 
-        chosen payment method. All payments are non-refundable unless otherwise stated or required by applicable law.
-      </p>
-    </li>
-    <li>
-      <h2>Digital Catalog and Cart</h2>
-      <p>
-      The website provides a digital catalog where users can browse and select products for purchase. Adding items to the cart does 
-      not guarantee availability or pricing of the products. The website reserves the right to modify or remove any product from the 
-      catalog without prior notice. The website is not responsible for any errors or inaccuracies in the product descriptions or pricing.
-      </p>
-    </li>
-    <li>
-      <h2>Contact Information</h2>
-      <p>
-      The website provides contact information, including email and phone number, for users to get in touch with Charlotte Imports / 
-      THORSMEX, S.A de C.V. Any communication initiated using the provided contact information should be lawful and respectful. Charlotte 
-      Imports / THORSMEX, S.A de C.V. reserves the right to use the provided contact information to respond to inquiries or for any other legitimate business purposes.
-      </p>
-    </li>
-    <li>
-      <h2>Intellectual Property</h2>
-      <p>
-      All content on the website, including but not limited to text, graphics, logos, images, and software, is the property of Charlotte Imports / THORSMEX, S.A de C.V. or 
-      its licensors and is protected by intellectual property laws. You may not reproduce, distribute, modify, transmit, or use any of the website's content without prior 
-      written permission from Charlotte Imports / THORSMEX, S.A de C.V.
-      </p>
-    </li>
-    </ol>
+      <div className="terms-privacy-content">
+        <h1>Terms of Use</h1>
+        <ol>
+          <li>
+            <h2>Acceptance of Terms</h2>
+            <p>
+              By accessing and using the Charlotte Imports / THORSMEX, S.A de C.V. Digital Catalog website
+              (hereinafter referred to as "the website"), you agree to comply with and be bound by the following
+              terms and conditions of use. If you do not agree with any part of these terms, please refrain from using the website.
+            </p>
+          </li>
+          <li>
+            <h2>Account Creation</h2>
+            <p>
+              To access certain features of the website, you may be required to create an account. You are solely responsible for
+              maintaining the confidentiality of your account information and for all activities that occur under your account. You
+              must provide accurate and complete information during the account registration process. You are responsible for keeping
+              your account information up to date and for promptly notifying us of any unauthorized use of your account or any other security breach.
+            </p>
+          </li>
+          <li>
+            <h2>Payment and Transactions</h2>
+            <p>
+              The website may offer a payment feature to facilitate the purchase of products or services. By making a payment, you
+              agree to provide accurate and complete payment information and authorize the website to charge the specified amount to your
+              chosen payment method. All payments are non-refundable unless otherwise stated or required by applicable law.
+            </p>
+          </li>
+          <li>
+            <h2>Digital Catalog and Cart</h2>
+            <p>
+              The website provides a digital catalog where users can browse and select products for purchase. Adding items to the cart does
+              not guarantee availability or pricing of the products. The website reserves the right to modify or remove any product from the
+              catalog without prior notice. The website is not responsible for any errors or inaccuracies in the product descriptions or pricing.
+            </p>
+          </li>
+          <li>
+            <h2>Contact Information</h2>
+            <p>
+              The website provides contact information, including email and phone number, for users to get in touch with Charlotte Imports /
+              THORSMEX, S.A de C.V. Any communication initiated using the provided contact information should be lawful and respectful. Charlotte
+              Imports / THORSMEX, S.A de C.V. reserves the right to use the provided contact information to respond to inquiries or for any other legitimate business purposes.
+            </p>
+          </li>
+          <li>
+            <h2>Intellectual Property</h2>
+            <p>
+              All content on the website, including but not limited to text, graphics, logos, images, and software, is the property of Charlotte Imports / THORSMEX, S.A de C.V. or
+              its licensors and is protected by intellectual property laws. You may not reproduce, distribute, modify, transmit, or use any of the website's content without prior
+              written permission from Charlotte Imports / THORSMEX, S.A de C.V.
+            </p>
+          </li>
+        </ol>
+      </div>
     </div>
   );
 }
