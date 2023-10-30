ebb54a03c565167d302d72c8c2f839fd12a8e5ae
jakeearing <jakeearing7@gmail.com>
2023-10-30 16:08:25 -0400

    Updated Terms of Use and Privacy Policy

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -5,37 +5,160 @@ export default function PrivacyPolicy() {
   return (
     <div className="terms-privacy-container">
       <div className="terms-privacy-content">
-        <h1>Privacy Policy</h1>
+        <h1>PRIVACY POLICY</h1>
         <ol>
           <li>
-            <h2>Collection and Use of Personal Information</h2>
+            <h2>Introduction</h2>
             <p>
-              The website may collect personal information such as name, email address, phone number, and shipping address for
-              the purpose of creating and managing user accounts, processing transactions, and providing customer support. We may
-              also collect non-personal information, such as IP addresses and browser information, to analyze website usage and improve our services.
+              <b>(a) Web Site Owner.</b> Charlotte Imports, LLC, (“CIMPO") is the owner of this web site. CIMPO can be contacted by phone at 1-800-950-0860,
+              or by e-mail at sales@charlotte-imports.com. This online privacy notice discloses CIMPO information practices for this CIMPO Web Site,
+              including what type of personal identifiable information is requested in order to make a purchase, how the information is used, and with
+              whom the in-formation is shared.
+            </p>
+            <p>
+              <b>(b) Web Site Visits</b> In general, you can visit on the CIMPO Web Site without disclosing or revealing any personal information.
+              CIMPO does not keep track of the domains from which people visit us.
+            </p>
+            <p>
+              <b>(c) Web Site Transactions.</b> At times, CIMPO will need personal information regarding a customer or a prospect. For example to process an order,
+              CIMPO may need to know a customer's name, mailing address, e-mail address and/or credit card details. It is CIMPO's intent to inform you know before
+              CIMPO collects personal information, such as user's name and/or address on the Internet. If you tell us that you do not wish to have this information
+              used as a basis for further contact with you, CIMPO will respect your wishes.
             </p>
           </li>
           <li>
-            <h2>Security Measures</h2>
+            <h2>Personal Information That May Be Collected.</h2>
             <p>
-              The website employs industry-standard security measures to protect against unauthorized access, alteration, disclosure, or
-              destruction of personal information. However, no method of transmission over the internet or electronic storage is 100%
-              secure, and we cannot guarantee absolute security of your personal information.
+              <b>(a) Identifying Information. </b>
+              In order to make a purchase, CIMPO will request a user to provide certain personal identifying information, which may include: name, postal address, e-mail address, telephone number, facsimile number, method of payment, and, if applicable, credit card number. CIMPO may request additional information necessary to establish and maintain customer's account.
+            </p>
+            <p>
+              <b>(b) Service Quality Monitoring. </b>
+              Some Web site transactions may require a customer to telephone CIMPO, or CIMPO to call the customer. CIMPO will not contact you by telephone without your prior consent, except to confirm an order placed online and/or to inform a customer of the status of such order.
+            </p>
+            <p>
+              <b>(c) Information from Children. </b>
+              CIMPO does not sell products or services for purchase by children, and will not knowingly collect or post information from a child under the age of 16 without the involvement of a parent or guardian. CIMPO will notify the child's parent or guardian at the e-mail address provided by the prospective customer, alerting the parent or guardian to the child's use of the Web site and providing instructions as to how the parent or guardian can delete the child's registration from the Web site.
+            </p>
+            <p>
+              <b>(d) Lost or Stolen Information. </b>
+              If a customer's credit card and/or password is lost or stolen, the customer should promptly notify CIMPO in order to enable CIMPO to cancel the lost or stolen information and to update its records with the new information.
+            </p>
+            <p>
+              <b>(e) Links to Other Web Sites. </b>
+              An CIMPO Web site may contain links to other web sites. CIMPO is not responsible for the privacy practices or the content of other Web sites.
             </p>
           </li>
+
           <li>
-            <h2>Cookies and Tracking Technologies</h2>
+            <h2>Uses Made of the Information.</h2>
             <p>
-              The website may use cookies and similar tracking technologies to enhance user experience, personalize content, and analyze
-              website traffic. You can adjust your browser settings to refuse cookies or provide notification when cookies are being sent.
-              However, certain website features may not function properly without cookies.
+              <b>(a) Limited Uses Identified. </b>
+              Without customer's prior consent, CIMPO will not use your personal identifiable information for any purpose other than that for which it is submitted. CIMPO uses personal identifiable information to reply to inquiries, handle complaints, provide operational notices and in program record-keeping. CIMPO also processes billing and business requests related to CIMPO Web Site participation.
+            </p>
+            <p>
+              <b>(b) Marketing Uses. </b>
+              Unless customer indicates otherwise, CIMPO reserves the right to provide customer with information about CIMPO's Web site, CIMPO products and services, and related information in which customer has indicated an interest.
+            </p>
+            <p>
+              <b>(c) Stored Information Uses. </b>
+              CIMPO retains the information entered on the CIMPO Web site. This information is used by CIMPO to support customer interaction with the CIMPO Web site; to deliver customer purchases; and/or to contact customer again about other CIMPO services and products.
             </p>
           </li>
+
           <li>
-            <h2>Third-Party Links</h2>
+            <h2>Disclosure of the Information.</h2>
             <p>
-              The website may contain links to third-party websites or services. Charlotte Imports / THORSMEX, S.A de C.V. is not responsible
-              for the privacy practices or content of these third-party sites. We encourage you to review the privacy policies of those sites.
+              <b>(a) Within Corporate Organization. </b>
+              CIMPO is a multinational organization, with legal entities, business processes, management structures, and technical systems that cross borders. CIMPO may share your personal information within the CIMPO corporate organization, and may transfer the information to countries in the world where CIMPO conducts business. Some countries may provide less legal protection for customer personal information. In such countries CIMPO will still handle customer personal information in the manner describe herein.
+            </p>
+            <p>
+              <b>(b)Mergers and Acquisitions. </b>
+              Circumstances may arise where for business reasons, CIMPO decides to sell, buy, merge or otherwise reorganize its businesses in the United States or some other country. Such a transaction may involve the disclosure of personal identifying information to prospective or actual purchasers, and/or receiving such information from sellers. It is CIMPO's practice to seek appropriate protection for information in these types of transactions.
+            </p>
+            <p>
+              <b>(c) Agents. </b>
+              CIMPO may occasionally employ or engage other companies and individuals to perform business functions on behalf of CIMPO. These persons are provided with personal identifying information required to perform their functions but are prohibited by contract from using the information for other purposes. These persons engage in a variety of functions which include, but are not limited to, fulfilling orders, delivering packages, removing repetitive information from customer lists, analyzing data, providing marketing assistance, processing credit card payments and providing customer services.
+            </p>
+            <p>
+              <b>(d) Affiliated Businesses. </b>
+              CIMPO works closely with affiliated businesses operating web site stores, providing services or selling products on each other's Web sites. Customer information related to a transaction with an affiliated business may be shared with that affiliated business.
+            </p>
+            <p>
+              <b>(e) Marketing Analysis by Third Parties. </b>
+              CIMPO reserves the right to disclose to third parties personal information about customers for marketing analysis; however, any
+              information disclosed will be in the form of aggregate data that does not describe or identify an individual customer.
+            </p>
+            <p>
+              <b>(f) Disclosure to Governmental Authorities. </b>
+              Under certain circumstances, personal information may be subject to disclosure pursuant to a judicial or other government subpoenas, warrants or orders.
+            </p>
+          </li>
+
+          <li>
+            <h2>Use of Computer Tracking Technologies.</h2>
+            <p>
+              <b>(a) No Tracking of Personal Information. </b>
+              CIMPO's Web Site is not set up to track, collect or distribute personal information not entered by visitors. Through web site access logs CIMPO does collect clickstream data and HTTP protocol elements, which generate certain kinds of non-identifying site usage data, such as the number of hits and visits to our sites. This information is used for internal purposes by technical support staff for research and development, user analysis and business decision making, all of which provides better services to the public. These statistics, which contain no personal information and cannot be used to gather such information, may also be provided to third parties.
+            </p>
+            <p>
+              <b>(b) Use of Cookies. </b>
+              CIMPO, or its third party vendors, collects non-identifiable and personal information through the use of various technologies, including "cookies". A cookie is an alphanumeric identifier that a Web site can transfer to customer's hard drive through customer's browser. The cookie is then stored on customer's computer as an anonymous tag that identifies the customer's computer, but not the customer. Cookies may be sent by CIMPO or its third party vendors. Customer can set its browser to notify customer before a cookie is received, giving an opportunity to decide whether to accept the cookie. Customer may also set its browser to turn off cookies; however, some Web sites may not then work properly.
+            </p>
+          </li>
+
+          <li>
+            <h2>Information Security.</h2>
+            <p>
+              <b>(a) Commitment to Online Security. </b>
+              CIMPO employs physical, electronic and managerial procedures to safeguard the security and integrity of personal information. Billing and payment data is encrypted whenever transmitted or received online. Personal information is accessible only by staff designated to handle online requests or complaints.
+            </p>
+            <p>
+              <b>(b) No Liability for Acts of Third Parties. </b>
+              CIMPO will exercise all reasonable efforts to safeguard the confidentiality of customer personal information. However, transmissions protected by industry standard security technology and implemented by human beings cannot be made absolutely secure. Consequently, CIMPO shall not be liable for unauthorized disclosure of personal information due to no fault of CIMPO including, but not limited to, errors in transmission and unauthorized acts of CIMPO staff and/or third parties.
+            </p>
+          </li>
+
+          <li>
+            <h2>Privacy Policy Changes and Opt-Out Rights.</h2>
+            <p>
+              <b>(a) Changes to Privacy Policy. </b>
+              This privacy notice was last updated on October 16, 2023. CIMPO reserves the right to change or update its privacy policy statement at any time.
+            </p>
+            <p>
+              <b>(b) Opt-Out Right. </b>
+              Customer has the right at any time to cease permitting personal information to be collected, used or disclosed by CIMPO and/or by any third parties with whom CIMPO has shared and/or transferred such personal information. Right of cancellation may be exercised by contacting CIMPO via e-mail or telephone. After processing the cancellation, CIMPO will delete customer or prospective customer's personal information from its data base.
+            </p>
+          </li>
+
+          <li>
+            <h2>Access Rights to Data.</h2>
+            <p>
+              <b>(a) Information Maintained by CIMPO. </b>
+              Upon customer's request, CIMPO will provide a reasonable description of customer's personally identifiable information that CIMPO maintains in its data bank.
+            </p>
+            <p>
+              <b>(b) Corrections and Changes to Personal Information. </b>
+              If customer's personal information changes, or if customer notes an error upon review of customer information that CIMPO has on file, please promptly e-mail CIMPO at sales@charlotte-imports.com and provide the new or correct information.
+            </p>
+            <p>
+              California’s privacy laws require a company to provide notice to California users of their rights to receive information on to which entities their information was shared for marketing purposes.
+            </p>
+            <p>
+              <b>(c) Your California Privacy Rights. </b>
+              Beginning on January 1, 2005, California Civil Code Section 1798.83 permits customers of CIMPO who are California residents to request certain information regarding CIMPO’s disclosure of personal information for their direct marketing purposes. To make such a request, please email sales@charlotte-imports.com. Within 30 days of receiving such a request, CIMPO will provide a list of the categories of personal information disclosed to third parties for third-party direct marketing purposes during the immediately preceding calendar year, along with the names and addresses of these third parties. This request may be made no more than once per calendar year. CIMPO reserves its right not to respond to requests submitted other than to the address specified in this paragraph.
+            </p>
+          </li>
+
+          <li>
+            <h2>Accountability.</h2>
+            <p>
+              <b>(a) Questions, Problems and Complaints. </b>
+              If you have a question about this policy statement, or a complaint about CIMPO compliance with this privacy policy, you may contact CIMPO by e-mail at sales@charlotte-imports.com.
+            </p>
+            <p>
+              <b>(b) Terms of Use. </b>
+              If customer chooses to enter into a purchase or sales order, customer's action is hereby deemed acceptance of CIMPO practices described in this policy statement. Any dispute over privacy between customer and CIMPO is subject to the provisions of this notice and to CIMPO's Terms of Use Agreement or Terms of Sale Agreement, as appropriate, which is hereby incorporated herein.
             </p>
           </li>
         </ol>
