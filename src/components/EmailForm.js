964ea1f514ced95a5bc5e11d3a05c2a3da12e9a8
jakeearing <jakeearing7@gmail.com>
2023-11-03 03:07:59 -0400

    Changed alert text when email is sent successfully.

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -36,7 +36,7 @@ export default function ClaimForm() {
         })
             .then((response) => {
                 if (response.ok) {
-                    alert('Thank you for reaching out. We\'ll get back to you in 24 hours.');
+                    alert('Thank you for contact Charlotte Imports. We\'ll get back to you within 24 hours!');
                     console.log('Email sent!');
                 } else {
                     console.log('Error sending email');
